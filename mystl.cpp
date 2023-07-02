f5ad7fe3fdd289dce4702756103fbe8eb99885f5
skyera <zgliu2@gmail.com>
2023-07-01 20:07:32 -0700

    more

--- mystl.cpp
+++ mystl.cpp
@@ -5,11 +5,14 @@
 #include "nanobench.h"
 #include "fakeit.hpp"
 #include <algorithm>
+#include <stdexcept>
 #include <iostream>
 #include <iterator>
 #include <string>
 #include <vector>
 #include <set>
+#include <limits>
+#include <thread>
 
 int fact(int n) {
     return n <= 1 ? n : fact(n - 1) * n;
@@ -19,6 +22,43 @@ struct SomeInterface {
     virtual int foo(int) = 0;
     virtual int bar(std::string) = 0;
 };
+
+template<class T>
+inline const T& max(const T& a, const T& b)
+{
+    return a < b ? b : a;
+}
+
+class Q {
+public:
+    typedef int SubType;
+};
+
+template <class T>
+class MyClass {
+private:
+    T value;
+public:
+    // copy ctor with implicit type conversion
+    template<class U>
+    MyClass(const MyClass<U>& x) {
+        value = x.getValue();
+    }
+
+    MyClass() {
+    }
+
+    template<class X>
+    void assign(const MyClass<X>& x) {
+        value = x.getValue();
+    }
+
+    T getValue() const {
+        return value;
+    }
+};
+
+enum struct Visible {True, False};
 
 TEST_CASE("testing the factorial function") {
     CHECK(fact(0) == 1); // should fail
@@ -158,4 +198,116 @@ TEST_CASE("mem_fun_ref") {
                 std::ostream_iterator<std::string::size_type>(std::cout, "\n"),
                 StringSize());
     }
+}
+
+TEST_CASE("max") {
+    int a = 3;
+    int b = 4;
+    int result = max(a, b);
+    REQUIRE(result == 4);
+}
+
+void throw_logic_error()
+{
+    std::string err_msg("error");
+    throw std::logic_error(err_msg);
+}
+
+TEST_CASE("logic_error") {
+    REQUIRE_THROWS_AS(throw_logic_error(), std::logic_error);
+}
+
+TEST_CASE("MyClass") {
+    MyClass<double> d;
+    MyClass<int> i;
+
+    d.assign(d);
+    d.assign(i);
+
+    MyClass<double> xd;
+    MyClass<double> xd2(xd);
+    MyClass<int> xi(xd);
+}
+
+TEST_CASE("explicit_init") {
+    int i1;
+    int i2 = int();
+    CHECK(i1 != 0);
+    CHECK(i2 == 0);
+}
+
+TEST_CASE("limits") {
+    std::cout << std::boolalpha;
+
+    std::cout << "max(short): " << std::numeric_limits<short>::max() <<
+        std::endl;
+    std::cout << "max(int): " << std::numeric_limits<int>::max() << std::endl;
+    std::cout << "max(long): " << std::numeric_limits<long>::max() << std::endl;
+    std::cout << "max(float): " << std::numeric_limits<float>::max() <<
+        std::endl;
+    std::cout << "max(double): " << std::numeric_limits<double>::max()
+        << std::endl;
+    std::cout << "max(long double): " << std::numeric_limits<long double>::max()
+        << std::endl;
+    std::cout << "is_signed(char): " << std::numeric_limits<char>::is_signed <<
+        std::endl;
+    std::cout << "is_specialized(string): "
+        << std::numeric_limits<std::string>::is_specialized << std::endl;
+}
+
+bool int_ptr_less(int* a, int* b)
+{
+    return *a < *b;
+}
+
+TEST_CASE("max") {
+    int x = 17;
+    int y = 42;
+    int* px = &x;
+    int* py = &y;
+    int *pmax;
+
+    pmax = std::max(px, py, int_ptr_less);
+    REQUIRE(*pmax == y);
+}
+
+TEST_CASE("max2") {
+    int x = 3;
+    long y = 4;
+
+    int result = std::max<long>(x, y);
+    REQUIRE(result == y);
+}
+
+TEST_CASE("enum") {
+    Visible v = Visible::True;
+
+    std::cout << "v: " << (int)v << std::endl;
+}
+
+TEST_CASE("atomic") {
+    int y = 0;
+    std::atomic<int> x(0);
+    ankerl::nanobench::Bench().run("compare_exchange_strong", [&] {
+            x.compare_exchange_strong(y, 0);
+            });
+}
+
+TEST_CASE("tutorial_fast_v2") {
+    uint64_t x = 1;
+    ankerl::nanobench::Bench().run("++x", [&]() {
+        ankerl::nanobench::doNotOptimizeAway(x += 1);
+    });
+}
+
+TEST_CASE("tutorial_slow_v1") {
+    ankerl::nanobench::Bench().run("sleep 100ms, auto", [&] {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    });
+}
+
+TEST_CASE("tutorial_slow_v2") {
+    ankerl::nanobench::Bench().epochs(3).run("sleep 100ms", [&] {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    });
 }
